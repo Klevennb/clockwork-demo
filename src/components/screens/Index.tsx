5bc387370346427c12da2d0a5906028de637f1df
Matthew Kleven <matthew.kleven@traivefinance.com>
2024-09-14 19:02:01 -0500

    moved header and fleshed out index page

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,10 +1,24 @@
-import { useRef, useState } from 'react';
-import { Header } from '~/components/shared/Header';
+import { Flex, View, Text, Button } from '@aws-amplify/ui-react';
+import { Link } from 'react-router-dom';
 
 function Index() {
   return (
     <>
-      <Header />
+      <View color="darkorange" backgroundColor="darkorange" className="h-screen w-full">
+        <Flex alignItems="center" direction={'column'}>
+          <Flex direction="column">
+            <Text fontSize="10em">Check these</Text>
+              <Text fontWeight="bold" fontSize="10em" color="white">
+                Out
+              </Text>
+          </Flex>
+          <Link to="/day-planner">
+            <Button color="blue.80" backgroundColor={'white'}>
+              Get Started
+            </Button>
+          </Link>
+        </Flex>
+      </View>
     </>
   );
 }
